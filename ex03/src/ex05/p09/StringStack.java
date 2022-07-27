404b5826ae433d3d77f4aabda415dc4d60e46460
admin <admin@DESKTOP-0KT5JMA>
2022-07-27 16:18:56 +0900

    스택 연습문제 풀이

--- ex03/src/ex05/p09/StringStack.java
+++ ex03/src/ex05/p09/StringStack.java
@@ -1,29 +1,51 @@
 package ex05.p09;
 
-public class StringStack implements Stack{
+public class StringStack implements Stack
+{
+	private int top;
+	private int stackSize;
+	private String itemArray[];
+
+	public StringStack(int stackSize) {
+		top = -1;
+		this.stackSize = stackSize;
+		itemArray = new String[this.stackSize];
+	}
 
 	@Override
 	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
+		return top+1;
 	}
 
 	@Override
 	public int capacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return stackSize;
 	}
 
 	@Override
 	public String pop() {
-		// TODO Auto-generated method stub
-		return null;
+		if(top == -1) {
+			System.out.println("Deleting fail! Array Stack is empty!!");
+			return null;
+		}
+		else{ 
+			String str1 = itemArray[top];
+			top--;
+			return str1;	
+		}
 	}
 
 	@Override
 	public boolean push(String val) {
-		// TODO Auto-generated method stub
-		return false;
+		if(top == stackSize-1){
+			System.out.println("Inserting fail! Array Stack is full!!");
+			return false;
+		}
+		else{ 			
+			itemArray[++top] = val;
+			System.out.println("Inserted Item : " + val);
+			return true;
+		}
 	}
 
 }
